308dc2aa59d542f8648cda285ae9eb496f7c7f22
Maksym Samchenko <samchenkoms@gmail.com>
2026-01-07 16:43:52 +0100

    fix: allow Railway to inject PORT

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,7 +4,7 @@ const envShema = z.object({
   NODE_ENV: z
     .enum(["development", "production", "test"])
     .default("development"),
-  PORT: z.string().default("3000"),
+  PORT: z.string().optional(),
   DATABASE_URL: z.string(),
   SECRET_KEY: z.string().min(32, "Secret key must be at least 32 characters"),
 });
--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ dotenv.config();
 import app from "./app";
 import { env } from "./config/env";
 
-const port: string = env.PORT;
+const port: number = Number(env.PORT) || 3000;
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
