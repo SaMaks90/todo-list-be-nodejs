38dab9e0df777956a72e4fb90869dc512b7b0e53
Maksym Samchenko <samchenkoms@gmail.com>
2026-02-10 18:18:59 +0100

    Del: Deleted empty test file for task service.

--- src/services/task/__tests__/task.service.test.ts
+++
@@ -1,7 +0,0 @@
-// import { getTasksInProject } from "../task.service";
-//
-// describe("task.service", () => {
-//   it("should get tasks by Project ID and return pagination response", async () => {
-//     const result = await getTasksInProject("");
-//   });
-// });
